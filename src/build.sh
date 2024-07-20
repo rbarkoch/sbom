21b70d4f83ed3b5a4548745fe6d7441b0b2cbfe1
Ronnie Bar-Kochba <ronnie.barkochba@gmail.com>
2024-07-20 15:17:09 +0000

    Added multi-platform, multi-arch building.

--- src/build.sh
+++ src/build.sh
@@ -2,4 +2,7 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-go build -o $SCRIPT_DIR/../bin/sbom.exe sbom.go
+env GOOS=linux   GOARCH=386   go build -o $SCRIPT_DIR/../bin/linux-386/sbom sbom.go
+env GOOS=linux   GOARCH=amd64 go build -o $SCRIPT_DIR/../bin/linux-amd64/sbom sbom.go
+env GOOS=windows GOARCH=386   go build -o $SCRIPT_DIR/../bin/windows-386/sbom.exe sbom.go
+env GOOS=windows GOARCH=amd64 go build -o $SCRIPT_DIR/../bin/windows-amd64/sbom.exe sbom.go
